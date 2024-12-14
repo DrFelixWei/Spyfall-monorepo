8d9ba1e53392dc26d507e10afcbb0769fd22ea1f
Felix Wei <felixwei7@gmail.com>
2024-12-13 19:28:25 -0800

    adding docker

--- server/src/websocket/game-io.adapter.ts
+++ server/src/websocket/game-io.adapter.ts
@@ -23,9 +23,30 @@ export class GameIoAdapter extends IoAdapter
   public bindClientConnect(server: any, callback: Function)
   {
     server.on(CONNECTION_EVENT, (socket: Socket) => {
-      console.log("New connection", socket.id);
+      console.log("New connection:", socket.id, ", from:", socket.handshake.headers.origin);
       socket.setMaxListeners(this.options.maxSocketListeners);
       callback(socket);
     });
   }
+
+  // public bindClientConnect(server: any, callback: Function) {
+  //   server.on(CONNECTION_EVENT, (socket: Socket) => {
+  //     const origin = socket.handshake.headers.origin;
+  
+  //     // Allowed origins from environment variables
+  //     const allowedOrigins = (process.env.CORS_ALLOW_ORIGIN || '').split(',');
+  
+  //     console.log('New connection:', socket.id, ', from:', origin);
+  
+  //     // Validate the origin
+  //     if (!allowedOrigins.includes(origin)) {
+  //       console.warn(`Connection from unauthorized origin: ${origin}`);
+  //       socket.disconnect(true); // Forcefully disconnect the socket
+  //       return;
+  //     }
+  
+  //     socket.setMaxListeners(this.options.maxSocketListeners);
+  //     callback(socket);
+  //   });
+  // }
 }
